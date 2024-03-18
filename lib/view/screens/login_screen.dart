40553fb4990d550aec368c143b203972685e9462
Ahmad Nour Haidar <ahmadnourhaidar@gmail.com>
2024-03-18 17:00:11 +0300

    add background image

--- lib/view/screens/login_screen.dart
+++ lib/view/screens/login_screen.dart
@@ -20,147 +20,159 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          'Login',
-          style: TextStyle(
-            fontWeight: FontWeight.w600,
-          ),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/background.jpg"),
+          fit: BoxFit.cover,
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30, 0, 10, 0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomTextFormField(
-              hintText: 'Email',
-              controller: emailController,
-              icon: Icons.email,
-              keyboardType: TextInputType.emailAddress,
-              validator: (value) {
-                if (value != null && value.isNotEmpty && value.contains('@')) {
-                  return null;
-                }
-                return 'Email is not valid';
-              },
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          centerTitle: true,
+          title: const Text(
+            'Login',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
             ),
-            const SizedBox(height: 16),
-            CustomTextFormField(
-              hintText: 'Password',
-              isPassword: true,
-              controller: passwordController,
-              icon: Icons.key,
-              keyboardType: TextInputType.visiblePassword,
-              validator: (value) {
-                if (value != null && value.length >= 6) {
-                  return null;
-                }
-                return 'Password is not strong';
-              },
-            ),
-            CheckboxListTile(
-              controlAffinity: ListTileControlAffinity.leading,
-              contentPadding: const EdgeInsets.symmetric(horizontal: 0.0),
-              checkColor: Colors.white,
-              activeColor: isSelectRememberMe ? color : Colors.white,
-              title: Text(
-                'Remember Me',
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 18,
-                  color: isSelectRememberMe ? color : Colors.black,
-                ),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(30, 0, 10, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomTextFormField(
+                hintText: 'Email',
+                controller: emailController,
+                icon: Icons.email,
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value != null &&
+                      value.isNotEmpty &&
+                      value.contains('@')) {
+                    return null;
+                  }
+                  return 'Email is not valid';
+                },
               ),
-              value: isSelectRememberMe,
-              onChanged: (value) {
-                setState(() {
-                  isSelectRememberMe = value ?? false;
-                });
-              },
-            ),
-            const SizedBox(height: 24),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: SizedBox(
-                width: MediaQuery.sizeOf(context).width - 80,
-                height: 50.0,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: color,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
+              const SizedBox(height: 16),
+              CustomTextFormField(
+                hintText: 'Password',
+                isPassword: true,
+                controller: passwordController,
+                icon: Icons.key,
+                keyboardType: TextInputType.visiblePassword,
+                validator: (value) {
+                  if (value != null && value.length >= 6) {
+                    return null;
+                  }
+                  return 'Password is not strong';
+                },
+              ),
+              CheckboxListTile(
+                controlAffinity: ListTileControlAffinity.leading,
+                contentPadding: const EdgeInsets.symmetric(horizontal: 0.0),
+                checkColor: Colors.white,
+                activeColor: isSelectRememberMe ? color : Colors.white,
+                title: Text(
+                  'Remember Me',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                    color: isSelectRememberMe ? color : Colors.black,
                   ),
-                  onPressed: () {
-                    if (emailController.text == email &&
-                        passwordController.text == password) {
-                      navigatorAndFinish(context, const HomePage());
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          backgroundColor: Colors.red,
-                          content: Text(
-                            'Email or Password Not Correct',
-                            style: TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.w600,
+                ),
+                value: isSelectRememberMe,
+                onChanged: (value) {
+                  setState(() {
+                    isSelectRememberMe = value ?? false;
+                  });
+                },
+              ),
+              const SizedBox(height: 24),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: SizedBox(
+                  width: MediaQuery.sizeOf(context).width - 80,
+                  height: 50.0,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: color,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    onPressed: () {
+                      if (emailController.text == email &&
+                          passwordController.text == password) {
+                        navigatorAndFinish(context, const HomePage());
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            backgroundColor: Colors.red,
+                            content: Text(
+                              'Email or Password Not Correct',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
                           ),
-                        ),
-                      );
-                    }
-                  },
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(
-                      letterSpacing: 1.0,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
-                      fontSize: 22,
+                        );
+                      }
+                    },
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        letterSpacing: 1.0,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                        fontSize: 22,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 24),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: TextButton(
-                onPressed: () {
-                  navigatorAndFinish(context, const SignupScreen());
-                },
-                child: const Text(
-                  'Don\'t have an account? Signup Now',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
+              const SizedBox(height: 24),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: TextButton(
+                  onPressed: () {
+                    navigatorAndFinish(context, const SignupScreen());
+                  },
+                  child: const Text(
+                    'Don\'t have an account? Signup Now',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 24),
-            // CustomCard(
-            //   onPressed: () {
-            //     print('onPressed');
-            //   },
-            //   restaurantName: 'Name',
-            //   partnerName: 'Partner\'s name',
-            //   age: 10,
-            // ),
-            // const SizedBox(height: 24),
-            // CustomCard(
-            //   onPressed: () {
-            //     print('onPressed');
-            //   },
-            //   restaurantName: 'Name',
-            //   partnerName: 'Partner\'s name',
-            //   age: 10,
-            // ),
-          ],
+              const SizedBox(height: 24),
+              // CustomCard(
+              //   onPressed: () {
+              //     print('onPressed');
+              //   },
+              //   restaurantName: 'Name',
+              //   partnerName: 'Partner\'s name',
+              //   age: 10,
+              // ),
+              // const SizedBox(height: 24),
+              // CustomCard(
+              //   onPressed: () {
+              //     print('onPressed');
+              //   },
+              //   restaurantName: 'Name',
+              //   partnerName: 'Partner\'s name',
+              //   age: 10,
+              // ),
+            ],
+          ),
         ),
       ),
     );
--- lib/view/screens/signup_screen.dart
+++ lib/view/screens/signup_screen.dart
@@ -27,209 +27,232 @@ class _SignupScreenState extends State<SignupScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          'Signup',
-          style: TextStyle(
-            fontWeight: FontWeight.w600,
-          ),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/background.jpg"),
+          fit: BoxFit.cover,
         ),
       ),
-      body: ListView(
-        padding: const EdgeInsets.fromLTRB(30, 0, 10, 0),
-        children: [
-          CustomTextFormField(
-            hintText: 'Name',
-            controller: nameController,
-            keyboardType: TextInputType.name,
-            validator: (value) {
-              if (value != null && value.length > 2) {
-                return null;
-              }
-              return 'Name is too short';
-            },
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          centerTitle: true,
+          title: const Text(
+            'Signup',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
           ),
-          const SizedBox(height: 16),
-          CustomTextFormField(
-            hintText: 'Age',
-            controller: ageController,
-            keyboardType: TextInputType.number,
-            validator: (value) {
-              if (value != null && value.isNotEmpty) {
-                final age = int.tryParse(value);
-                if (age != null && age > 1 && age <= 120) {
+        ),
+        body: ListView(
+          padding: const EdgeInsets.fromLTRB(30, 0, 10, 0),
+          children: [
+            CustomTextFormField(
+              hintText: 'Name',
+              controller: nameController,
+              keyboardType: TextInputType.name,
+              validator: (value) {
+                if (value != null && value.length > 2) {
                   return null;
                 }
-                return 'Age not correct';
-              }
-              return 'Age not correct';
-            },
-          ),
-          const SizedBox(height: 16),
-          CustomMenu(
-            title: gender ?? 'Gender',
-            items: const [
-              PopupMenuItemModel(title: 'Male', value: 'Male'),
-              PopupMenuItemModel(title: 'Female', value: 'Female'),
-            ],
-            onChange: (value) {
-              setState(() {
-                gender = value;
-              });
-            },
-          ),
-          const SizedBox(height: 16),
-          CustomMenu(
-            title: country ?? 'Country',
-            items: const [
-              PopupMenuItemModel(title: 'Syria', value: 'Syria'),
-              PopupMenuItemModel(title: 'Sweden', value: 'Sweden'),
-              PopupMenuItemModel(title: 'Germany', value: 'Germany'),
-            ],
-            onChange: (value) {
-              setState(() {
-                gender = value;
-              });
-            },
-          ),
-          const SizedBox(height: 16),
-          CustomMenu(
-            title: city ?? 'City',
-            items: const [
-              PopupMenuItemModel(title: 'Stockholm', value: 'Stockholm'),
-              PopupMenuItemModel(title: 'Gothenburg', value: 'Gothenburg'),
-              PopupMenuItemModel(title: 'Malmö', value: 'Malmö'),
-            ],
-            onChange: (value) {
-              setState(() {
-                gender = value;
-              });
-            },
-          ),
-          const SizedBox(height: 16),
-          CustomTextFormField(
-            hintText: 'Phone number',
-            controller: phoneNumberController,
-            keyboardType: TextInputType.number,
-            validator: (value) {
-              if (value != null && value.length == 10) {
-                return null;
-              }
-              return 'Phone number is not valid';
-            },
-          ),
-          const SizedBox(height: 16),
-          CustomTextFormField(
-            hintText: 'Email',
-            controller: emailController,
-            icon: Icons.email,
-            keyboardType: TextInputType.emailAddress,
-            validator: (value) {
-              if (value != null && value.isNotEmpty && value.contains('@')) {
-                return null;
-              }
-              return 'Email is not valid';
-            },
-          ),
-          const SizedBox(height: 16),
-          CustomTextFormField(
-            hintText: 'Password',
-            isPassword: true,
-            controller: passwordController,
-            icon: Icons.key,
-            keyboardType: TextInputType.visiblePassword,
-            validator: (value) {
-              if (value != null && value.length >= 6) {
-                return null;
-              }
-              return 'Password is not strong';
-            },
-          ),
-          const SizedBox(height: 16),
-          CustomTextFormField(
-            hintText: 'Reenter password',
-            isPassword: true,
-            controller: reenterPasswordController,
-            icon: Icons.key,
-            keyboardType: TextInputType.visiblePassword,
-            validator: (value) {
-              if (value != null &&
-                  value.length >= 6 &&
-                  passwordController.text == reenterPasswordController.text) {
-                return null;
-              }
-              return 'Password is not strong';
-            },
-          ),
-          CheckboxListTile(
-            controlAffinity: ListTileControlAffinity.leading,
-            contentPadding: const EdgeInsets.symmetric(horizontal: 0.0),
-            checkColor: Colors.white,
-            activeColor: isAccept ? color : Colors.white,
-            title: Text(
-              'Accept the Term & Services',
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 18,
-                color: isAccept ? color : Colors.black,
-              ),
+                return 'Name is too short';
+              },
             ),
-            value: isAccept,
-            onChanged: (value) {
-              setState(() {
-                isAccept = value ?? false;
-              });
-            },
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: SizedBox(
-              width: MediaQuery.sizeOf(context).width - 80,
-              height: 50.0,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: color,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+            const SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: 'Age',
+              controller: ageController,
+              keyboardType: TextInputType.number,
+              validator: (value) {
+                if (value != null && value.isNotEmpty) {
+                  final age = int.tryParse(value);
+                  if (age != null && age > 1 && age <= 120) {
+                    return null;
+                  }
+                  return 'Age not correct';
+                }
+                return 'Age not correct';
+              },
+            ),
+            const SizedBox(height: 16),
+            CustomMenu(
+              title: gender ?? 'Gender',
+              items: const [
+                PopupMenuItemModel(title: 'Male', value: 'Male'),
+                PopupMenuItemModel(title: 'Female', value: 'Female'),
+              ],
+              onChange: (value) {
+                setState(() {
+                  gender = value;
+                });
+              },
+            ),
+            const SizedBox(height: 16),
+            CustomMenu(
+              title: country ?? 'Country',
+              items: const [
+                PopupMenuItemModel(title: 'Syria', value: 'Syria'),
+                PopupMenuItemModel(title: 'Sweden', value: 'Sweden'),
+                PopupMenuItemModel(title: 'Germany', value: 'Germany'),
+              ],
+              onChange: (value) {
+                setState(() {
+                  gender = value;
+                });
+              },
+            ),
+            const SizedBox(height: 16),
+            CustomMenu(
+              title: city ?? 'City',
+              items: const [
+                PopupMenuItemModel(title: 'Stockholm', value: 'Stockholm'),
+                PopupMenuItemModel(title: 'Gothenburg', value: 'Gothenburg'),
+                PopupMenuItemModel(title: 'Malmö', value: 'Malmö'),
+              ],
+              onChange: (value) {
+                setState(() {
+                  gender = value;
+                });
+              },
+            ),
+            const SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: 'Phone number',
+              controller: phoneNumberController,
+              keyboardType: TextInputType.number,
+              validator: (value) {
+                if (value != null && value.length == 10) {
+                  return null;
+                }
+                return 'Phone number is not valid';
+              },
+            ),
+            const SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: 'Email',
+              controller: emailController,
+              icon: Icons.email,
+              keyboardType: TextInputType.emailAddress,
+              validator: (value) {
+                if (value != null && value.isNotEmpty && value.contains('@')) {
+                  return null;
+                }
+                return 'Email is not valid';
+              },
+            ),
+            const SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: 'Password',
+              isPassword: true,
+              controller: passwordController,
+              icon: Icons.key,
+              keyboardType: TextInputType.visiblePassword,
+              validator: (value) {
+                if (value != null && value.length >= 6) {
+                  return null;
+                }
+                return 'Password is not strong';
+              },
+            ),
+            const SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: 'Reenter password',
+              isPassword: true,
+              controller: reenterPasswordController,
+              icon: Icons.key,
+              keyboardType: TextInputType.visiblePassword,
+              validator: (value) {
+                if (value != null &&
+                    value.length >= 6 &&
+                    passwordController.text == reenterPasswordController.text) {
+                  return null;
+                }
+                return 'Password is not strong';
+              },
+            ),
+            CheckboxListTile(
+              controlAffinity: ListTileControlAffinity.leading,
+              contentPadding: const EdgeInsets.symmetric(horizontal: 0.0),
+              checkColor: Colors.white,
+              activeColor: isAccept ? color : Colors.white,
+              title: Text(
+                'Accept the Term & Services',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18,
+                  color: isAccept ? color : Colors.black,
+                ),
+              ),
+              value: isAccept,
+              onChanged: (value) {
+                setState(() {
+                  isAccept = value ?? false;
+                });
+              },
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: SizedBox(
+                width: MediaQuery.sizeOf(context).width - 80,
+                height: 50.0,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: color,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: () {
+                    email = emailController.text;
+                    password = passwordController.text;
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        backgroundColor: Colors.green,
+                        content: Text(
+                          'Success',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    );
+                    navigatorAndFinish(context, const LoginScreen());
+                  },
+                  child: const Text(
+                    'Signup',
+                    style: TextStyle(
+                      letterSpacing: 1.0,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                      fontSize: 22,
+                    ),
                   ),
                 ),
+              ),
+            ),
+            const SizedBox(height: 24),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: TextButton(
                 onPressed: () {
-                  email = emailController.text;
-                  password = passwordController.text;
+                  navigatorAndFinish(context, const LoginScreen());
                 },
                 child: const Text(
-                  'Signup',
+                  'Have an account? Login Now',
                   style: TextStyle(
-                    letterSpacing: 1.0,
+                    color: Colors.black,
+                    fontSize: 18,
                     fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                    fontSize: 22,
                   ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 24),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: TextButton(
-              onPressed: () {
-                navigatorAndFinish(context, const LoginScreen());
-              },
-              child: const Text(
-                'Have an account? Login Now',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 24),
-        ],
+            const SizedBox(height: 24),
+          ],
+        ),
       ),
     );
   }
